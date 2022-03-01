5e275c3bcc889fcb672d5e74d4f8ad6f5594e5d0
TrooperCrypto <trooper.crypto7@gmail.com>
2022-03-01 21:44:37 +0100

    priceChange is in lastprices

--- src/services/subscribemarket.ts
+++ src/services/subscribemarket.ts
@@ -12,9 +12,9 @@ export const subscribemarket: ZZServiceHandler = async (
     const yesterday = new Date(Date.now() - 86400 * 1000)
       .toISOString()
       .slice(0, 10)
-    let lastprice
+    let lastprice, priceChange
     try {
-      ;[, lastprice] = lastprices.find((l) => l[0] === market) as any
+      ;[, lastprice, priceChange] = lastprices.find((l) => l[0] === market) as any
     } catch (e) {
       console.error(`No price found for ${market}`)
       lastprice = 0
@@ -25,14 +25,6 @@ export const subscribemarket: ZZServiceHandler = async (
     const yesterdayPrice = Number(
       await api.redis.get(`dailyprice:${chainid}:${market}:${yesterday}`)
     )
-    let priceChange
-    if (yesterdayPrice) {
-      priceChange = (lastprice - yesterdayPrice).toFixed(
-        marketinfo.pricePrecisionDecimals
-      )
-    } else {
-      priceChange = 0
-    }
     const hi24 = Math.max(lastprice, yesterdayPrice)
     const lo24 = Math.min(lastprice, yesterdayPrice)
     const marketSummaryMsg = {
