753ec9df3b5d83e212cb6036be0b255f88615c77
TrooperCrypto <trooper.crypto7@gmail.com>
2022-03-02 14:38:14 +0100

    update subscribemarket
    - use getMarketSummarys
    - remove lastprice
    - change order -> get Info, send Info

--- src/services/subscribemarket.ts
+++ src/services/subscribemarket.ts
@@ -1,64 +1,47 @@
-import type { ZZServiceHandler } from 'src/types'
+import type { ZZServiceHandler, ZZMarketSummary} from 'src/types'
 
 export const subscribemarket: ZZServiceHandler = async (
   api,
   ws,
   [chainid, market]
 ) => {
-  const openorders = await api.getopenorders(chainid, market)
-  const fills = await api.getfills(chainid, market)
-  const lastprices = await api.getLastPrices(chainid)
   try {
-    const yesterday = new Date(Date.now() - 86400 * 1000)
-      .toISOString()
-      .slice(0, 10)
-    let lastprice, priceChange
-    try {
-      ;[, lastprice, priceChange] = lastprices.find((l) => l[0] === market) as any
-    } catch (e) {
-      console.error(`No price found for ${market}`)
-      lastprice = 0
-      priceChange = 0
-    }
-    const marketinfo = await api.getMarketInfo(market, chainid)
-    const baseVolume = await api.redis.HGET(`volume:${chainid}:base`, market)
-    const quoteVolume = await api.redis.HGET(`volume:${chainid}:quote`, market)
-    const yesterdayPrice = Number(
-      await api.redis.get(`dailyprice:${chainid}:${market}:${yesterday}`)
-    )
-    const hi24 = Math.max(lastprice, yesterdayPrice).toFixed(
-      marketinfo.pricePrecisionDecimals
-    )
-    const lo24 = Math.min(lastprice, yesterdayPrice).toFixed(
-      marketinfo.pricePrecisionDecimals
-    )
+    const marketSummary: ZZMarketSummary = (await api.getMarketSummarys(
+      chainid,
+      market
+    ))[0]
+    const marketinfo = await api.getMarketInfo(market, chainid)  
     const marketSummaryMsg = {
       op: 'marketsummary',
       args: [
-        market,
-        lastprice,
-        hi24,
-        lo24,
-        priceChange,
-        baseVolume,
-        quoteVolume,
+        marketSummary.market,
+        marketSummary.lastPrice,
+        marketSummary.highestPrice_24h,
+        marketSummary.lowestPrice_24h,
+        marketSummary.priceChange,
+        marketSummary.baseVolume,
+        marketSummary.quoteVolume,
       ],
     }
     ws.send(JSON.stringify(marketSummaryMsg))
     const marketInfoMsg = { op: 'marketinfo', args: [marketinfo] }
-    ws.send(JSON.stringify(marketInfoMsg))
+    ws.send(JSON.stringify(marketInfoMsg))  
   } catch (e) {
     console.error(e)
   }
-  ws.send(JSON.stringify({ op: 'lastprice', args: [lastprices] }))
+  
+  const openorders = await api.getopenorders(chainid, market)
   ws.send(JSON.stringify({ op: 'orders', args: [openorders] }))
+
+  const fills = await api.getfills(chainid, market)
   ws.send(JSON.stringify({ op: 'fills', args: [fills] }))
+
   if ([1, 1000].includes(chainid)) {
     const liquidity = await api.getLiquidity(chainid, market)
     ws.send(
       JSON.stringify({ op: 'liquidity2', args: [chainid, market, liquidity] })
     )
-  }
+  }  
   ws.chainid = chainid
   ws.marketSubscriptions.push(market)
 }
