6f9f581b15b2b40bf4cc8812115fd0f6787bae58
TrooperCrypto <trooper.crypto7@gmail.com>
2022-04-06 18:49:21 +0200

    disable for now

--- src/services/submitorder2.ts
+++ src/services/submitorder2.ts
@@ -18,8 +18,9 @@ export const submitorder2: ZZServiceHandler = async (
     }
   } else if (chainId === 1001) {
     try {
-      const order = await api.processorderstarknet(chainId, market, zktx)
-      return order
+      // const order = await api.processorderstarknet(chainId, market, zktx)
+      // return order
+      throw new Error ("StarkNet not supported for now.")
     } catch (err: any) {
       console.error(err)
       const errorMsg = { op: 'error', args: ['submitorder2', err.message] }
