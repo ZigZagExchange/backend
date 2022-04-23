5536093ba594ce308db1ecb6ad3d4c90b8c795a5
TrooperCrypto <trooper.crypto7@gmail.com>
2022-04-23 22:51:13 +0200

    less logs for submitorder2/3

--- src/socketServer.ts
+++ src/socketServer.ts
@@ -27,7 +27,18 @@ export const createSocketServer = (): ZZSocketServer => {
       try {
         msg = JSON.parse(json) as WSMessage
         if (typeof msg.op === 'string' && Array.isArray(msg.args)) {
-          if (msg.op !== 'indicateliq2') console.log('WS: %s', json)
+          if (![
+            'indicateliq2',
+            'submitorder2',
+            'submitorder3'
+          ].includes(msg.op)) { console.log('WS: %s', json) }
+          if ([
+            'submitorder2',
+            'submitorder3'
+          ].includes(msg.op)) {
+            console.log(`WS: {"op":"submitorder2","args":[${msg.args[0]},${msg.args[1]}, "ZZMessage"]}`)
+          }
+
           if (wss.api) return wss.api.serviceHandler(msg, ws)
         }
       } catch (err) {
