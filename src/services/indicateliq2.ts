3f7ff3d67fe69e1379f1200cc9d590596841015c
TrooperCrypto <trooper.crypto7@gmail.com>
2022-05-25 14:01:59 +0200

    improve liquidity return

--- src/services/indicateliq2.ts
+++ src/services/indicateliq2.ts
@@ -8,7 +8,13 @@ export const indicateliq2: ZZServiceHandler = async (
   const makerConnId = `${chainId}:${ws.uuid}`
   api.MAKER_CONNECTIONS[makerConnId] = ws
   try {
-    await api.updateLiquidity(chainId, market, liquidity, ws.uuid)
+    const errorMsg = await api.updateLiquidity(chainId, market, liquidity, ws.uuid)
+    
+    // return any bad liquidity msg
+    if (errorMsg.length > 0) {
+      const errorString = `Send one or more invalid liquidity positions: ${errorMsg.join('. ')}.`
+      ws.send(JSON.stringify({ op: 'error', args: ['indicateliq2', errorString] }))
+    }
   } catch (e: any) {
     ws.send(JSON.stringify({ op: 'error', args: ['indicateliq2', e.message] }))
   }
