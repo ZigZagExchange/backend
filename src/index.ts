238c63e1a66105bf3f8a50aca4f357c64140f3be
TrooperCrypto <trooper.crypto7@gmail.com>
2022-05-25 15:15:51 +0200

    add throng

--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,30 @@ import { redis, subscriber, publisher } from 'src/redisClient'
 import db from 'src/db'
 import API from 'src/api'
 import type { RedisClientType } from 'redis'
+import throng from 'throng'
 
 const socketServer = createSocketServer()
 const httpServer = createHttpServer(socketServer)
 
-const port = Number(process.env.PORT) || 3004
-const api = new API(
-  socketServer as any,
-  db,
-  httpServer,
-  redis as RedisClientType,
-  subscriber as RedisClientType,
-  publisher as RedisClientType
-)
 
-api.start(port).then(() => {
-  console.log('Successfully started server.')
-})
+function start () {
+  const port = Number(process.env.PORT) || 3004
+  const api = new API(
+    socketServer as any,
+    db,
+    httpServer,
+    redis as RedisClientType,
+    subscriber as RedisClientType,
+    publisher as RedisClientType
+  )
+  
+  api.start(port).then(() => {
+    console.log('Successfully started server.')
+  })
+}
+
+const WORKERS = process.env.WEB_CONCURRENCY || 1
+throng({
+  workers: WORKERS,
+  lifetime: Infinity
+}, start)
