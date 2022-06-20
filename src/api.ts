cb026ee5e5dcf121ac548db1a9b22d6643c933d9
Trooper <trooper.crypto7@gmail.com>
2022-06-20 19:57:00 +0200

    dont allow same wallet fills

--- src/api.ts
+++ src/api.ts
@@ -1147,6 +1147,10 @@ export default class API extends EventEmitter {
     }
 
     const selectresult = select.rows[0]
+    
+    if(selectresult.userid === fillOrder.accountId.toString()) {
+      throw new Error(`Selfe-swap is not allowed`)
+    }
 
     // Determine fill price
     const marketInfo = await this.getMarketInfo(selectresult.market, chainId)
