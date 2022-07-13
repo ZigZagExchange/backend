afb012648c288785ee625ef7cbd860c300970ba6
taureau <vixidev99@protonmail.com>
2022-07-12 19:36:39 -0700

    30 latest trades

--- src/background.ts
+++ src/background.ts
@@ -1125,7 +1125,7 @@ async function cacheRecentTrades() {
   const results0: Promise<any>[] = VALID_CHAINS_ZKSYNC.map(async (chainId) => {
     const markets = await redis.SMEMBERS(`activemarkets:${chainId}`)
     const results1: Promise<any>[] = markets.map(async (marketId) => {
-      const text = "SELECT chainid,id,market,side,price,amount,fill_status,txhash,taker_user_id,maker_user_id,feeamount,feetoken,insert_timestamp FROM fills WHERE chainid=$1 AND fill_status='f' AND market=$2 ORDER BY id LIMIT 30"
+      const text = "SELECT chainid,id,market,side,price,amount,fill_status,txhash,taker_user_id,maker_user_id,feeamount,feetoken,insert_timestamp FROM fills WHERE chainid=$1 AND fill_status='f' AND market=$2 ORDER BY id DESC LIMIT 30"
       const query = {
         text,
         values: [chainId, marketId],
