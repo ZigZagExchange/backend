f3db6abcd6aaaf3e19e45b4aef1c84d70cd0cbd3
taureau <vixidev99@protonmail.com>
2022-07-22 13:29:16 -0700

    recent trades for all chains

--- src/background.ts
+++ src/background.ts
@@ -1257,7 +1257,7 @@ async function seedArbitrumMarkets() {
 
 async function cacheRecentTrades() {
   console.time('cacheRecentTrades')
-  const results0: Promise<any>[] = VALID_CHAINS_ZKSYNC.map(async (chainId) => {
+  const results0: Promise<any>[] = VALID_CHAINS.map(async (chainId) => {
     const markets = await redis.SMEMBERS(`activemarkets:${chainId}`)
     const results1: Promise<any>[] = markets.map(async (marketId) => {
       const text = "SELECT chainid,id,market,side,price,amount,fill_status,txhash,taker_user_id,maker_user_id,feeamount,feetoken,insert_timestamp FROM fills WHERE chainid=$1 AND fill_status='f' AND market=$2 ORDER BY id DESC LIMIT 30"
