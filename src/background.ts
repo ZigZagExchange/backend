56a7a316aab1ac1c8992905be6e71c9b983686eb
taureau75 <vixidev99@protonmail.com>
2022-10-14 14:19:11 -0700

    trying one more time to get deleting rows to wrok

--- src/background.ts
+++ src/background.ts
@@ -1499,10 +1499,7 @@ async function checkEVMChainAllowance() {
 
 async function deleteOldOrders () {
   console.time('deleteOldOrders')
-  const query = {
-    text: "DELETE FROM offers WHERE order_status NOT IN ('o', 'pm', 'pf', 'b', 'm')",
-  }
-  await db.query(query)
+  await db.query("DELETE FROM offers WHERE order_status NOT IN ('o', 'pm', 'pf', 'b', 'm')")
   console.timeEnd('deleteOldOrders')
 }
 
