87703f3f6349d9b8f4b9194136de3aebc9883e00
taureau75 <vixidev99@protonmail.com>
2022-10-17 15:44:41 -0700

    simplified exchange contract with direct taker fills

--- evm_contracts/contracts/Exchange.sol
+++ evm_contracts/contracts/Exchange.sol
@@ -1,208 +1,160 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./LibOrder.sol";
-import "./LibFillResults.sol";
-import "./SignatureValidator.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import {EIP712} from '@openzeppelin/contracts/utils/cryptography/EIP712.sol';
+import {SignatureChecker} from '@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol';
 
 //import "hardhat/console.sol";
 
-contract Exchange is SignatureValidator{
+contract Exchange is EIP712 {
+  bytes32 internal constant _EIP712_ORDER_SCHEMA_HASH =
+    0x68d868c8698fc31da3a36bb7a184a4af099797794701bae97bea3de7ebe6e399;
+  //keccak256("Order(address user,address sellToken,address buyToken,uint256 sellAmount,uint256 buyAmount,uint256 expirationTimeSeconds)")
 
-    event Swap(address maker, address taker, address makerSellToken, address takerSellToken, uint makerSellAmount, uint takerSellAmount, uint gasFee, uint makerVolumeFee, uint takerVolumeFee);
+  struct Order {
+    address user; //address of the Order Creator making the sale
+    address sellToken; // address of the Token the Order Creator wants to sell
+    address buyToken; // address of the Token the Order Creator wants to receive in return
+    uint256 sellAmount; // amount of Token that the Order Creator wants to sell
+    uint256 buyAmount; // amount of Token that the Order Creator wants to receive in return
+    uint256 expirationTimeSeconds; //time after which the order is no longer valid
+  }
 
-    using LibOrder for LibOrder.Order;
+  event Swap(
+    address maker,
+    address taker,
+    address makerSellToken,
+    address takerSellToken,
+    uint256 makerSellAmount,
+    uint256 takerSellAmount,
+    uint256 makerVolumeFee,
+    uint256 takerVolumeFee
+  );
 
-    mapping (bytes32 => uint256) public filled;
+  mapping(bytes32 => uint256) public fills;
 
-    mapping (bytes32 => bool) public cancelled;
+  // fees
+  address FEE_ADDRESS;
+  uint256 maker_fee_numerator = 0;
+  uint256 maker_fee_denominator = 10000;
+  uint256 taker_fee_numerator = 0;
+  uint256 taker_fee_denominator = 10000;
 
-    // fees
-    address FEE_ADDRESS;
-    uint256 maker_fee_numerator = 0;
-    uint256 maker_fee_denominator = 10000;
-    uint256 taker_fee_numerator = 0;
-    uint256 taker_fee_denominator = 10000;
-  
-    // initialize fee address
-    constructor(address fee_address) {
-        FEE_ADDRESS = fee_address;    
+  // initialize fee address
+  constructor(
+    string memory name,
+    string memory version,
+    address fee_address
+  ) EIP712(name, version) {
+    FEE_ADDRESS = fee_address;
+  }
+
+  function cancelOrder(Order memory order) public {
+    require(msg.sender == order.user, 'only user may cancel order');
+    bytes32 orderHash = getOrderHash(order);
+    fills[orderHash] = order.sellAmount;
+  }
+
+  function matchOrder(
+    Order memory order,
+    bytes memory makerSignature,
+    uint fillAmount,
+    bool fillAvailable
+  )
+    public
+    returns (bool)
+  {
+
+    bytes32 orderhash = getOrderHash(order);
+
+    // adjust size if the user wants to fill whatever is available
+    uint availableSize = order.sellAmount - fills[orderhash];
+    if (fillAvailable && fillAmount > availableSize) {
+      fillAmount = availableSize;
+    } 
+    require(fillAmount <= availableSize, "fill amount exceeds available size");
+    require(block.timestamp <= order.expirationTimeSeconds, 'order expired');
+
+    require(
+      _isValidSignatureHash(
+        order.user,
+        orderhash,
+        makerSignature
+      ),
+      'invalid maker signature'
+    );
+
+
+    fills[orderhash] += fillAmount;
+
+    // Send out the tokens
+    uint buyFillAmount = order.buyAmount * fillAmount / order.sellAmount;
+    IERC20(order.sellToken).transferFrom(order.user, msg.sender, fillAmount);
+    IERC20(order.buyToken).transferFrom(msg.sender, order.user, buyFillAmount);
+
+    // Calculate and charge the fees
+    uint takerFee = buyFillAmount * taker_fee_numerator / taker_fee_denominator;
+    uint makerFee = fillAmount * maker_fee_numerator / maker_fee_denominator;
+    if (takerFee > 0) {
+      IERC20(order.buyToken).transferFrom(msg.sender, FEE_ADDRESS, takerFee);
+    }
+    if (makerFee > 0) {
+      IERC20(order.sellToken).transferFrom(order.user, FEE_ADDRESS, makerFee);
     }
 
-    function cancelOrder(
-        LibOrder.Order memory order
-    ) public{   
+    emit Swap(
+      order.user,
+      msg.sender,
+      order.sellToken,
+      order.buyToken,
+      fillAmount,
+      buyFillAmount,
+      makerFee,
+      takerFee
+    );
 
-        require(msg.sender == order.user, "only user may cancel order");
+    return true;
+  }
 
-        LibOrder.OrderInfo memory orderInfo = getOrderInfo(order);
-
-        cancelled[orderInfo.orderHash] = true;
-    }
+  // https://eips.ethereum.org/EIPS/eip-712#definition-of-hashstruct
+  function getOrderHash(Order memory order) internal pure returns (bytes32) {
+    bytes32 orderHash = keccak256(
+      abi.encode(
+        _EIP712_ORDER_SCHEMA_HASH,
+        order.user,
+        order.sellToken,
+        order.buyToken,
+        order.sellAmount,
+        order.buyAmount,
+        order.expirationTimeSeconds
+      )
+    );
     
-   function matchOrders(
-       LibOrder.Order memory makerOrder, 
-       LibOrder.Order memory takerOrder,
-       bytes memory makerSignature,
-       bytes memory takerSignature
-   )
-   public returns(LibFillResults.MatchedFillResults memory matchedFillResults){
+    return orderHash;
+  }
 
-        // check that tokens address match
-        require(takerOrder.sellToken == makerOrder.buyToken, "mismatched tokens");
-        require(takerOrder.buyToken == makerOrder.sellToken, "mismatched tokens");
-  
-        // check the relayer field
-        require(makerOrder.relayerAddress == address(0) || makerOrder.relayerAddress == msg.sender, "maker relayer mismatch");
-        require(takerOrder.relayerAddress == address(0) || takerOrder.relayerAddress == msg.sender, "taker relayer mismatch");
+  function _isValidSignatureHash(
+    address a,
+    bytes32 orderHash,
+    bytes memory signature
+  ) private view returns (bool) {
+    bytes32 digest = _hashTypedDataV4(orderHash);
 
-        LibOrder.OrderInfo memory makerOrderInfo = getOrderInfo(makerOrder);
-        LibOrder.OrderInfo memory takerOrderInfo = getOrderInfo(takerOrder);
-       
-     
-        require(takerOrderInfo.orderStatus == LibOrder.OrderStatus.FILLABLE, "taker order status not Fillable");
-        require(makerOrderInfo.orderStatus == LibOrder.OrderStatus.FILLABLE, "maker order status not Fillable");
+    return SignatureChecker.isValidSignatureNow(a, digest, signature);
+  }
 
-        //validate signature
-        require(msg.sender == takerOrder.user || _isValidOrderWithHashSignature(takerOrderInfo.orderHash, takerSignature, takerOrder.user), "invalid taker signature");
-        require(msg.sender == makerOrder.user || _isValidOrderWithHashSignature(makerOrderInfo.orderHash, makerSignature, makerOrder.user),"invalid maker signature");
-        
-        // Make sure there is a profitable spread.
-        // There is a profitable spread iff the cost per unit bought (OrderA.SellAmount/OrderA.BuyAmount) for each order is greater
-        // than the profit per unit sold of the matched order (OrderB.BuyAmount/OrderB.SellAmount).
-        // This is satisfied by the equations below:
-        // <makerOrder.sellAmount> / <makerOrder.buyAmount> >= <takerOrder.buyAmount> / <takerOrder.sellAmount>
-        // AND
-        // <takerOrder.sellAmount> / <takerOrder.buyAmount> >= <makerOrder.buyAmount> / <makerOrder.sellAmount>
-        // These equations can be combined to get the following:
-        require(
-            makerOrder.sellAmount * takerOrder.sellAmount >= makerOrder.buyAmount * takerOrder.buyAmount, 
-            "not profitable spread"
-        );
+  function setFees(
+    uint256 _taker_fee_numerator,
+    uint256 _taker_fee_denominator,
+    uint256 _maker_fee_numerator,
+    uint256 _maker_fee_denominator
+  ) public {
+    require(msg.sender == FEE_ADDRESS, 'only fee address may update fees');
 
-        matchedFillResults = LibFillResults.calculateMatchedFillResults(
-            makerOrder,
-            takerOrder,
-            makerOrderInfo.orderBuyFilledAmount,
-            takerOrderInfo.orderBuyFilledAmount,    
-            maker_fee_numerator,
-            maker_fee_denominator,
-            taker_fee_numerator,
-            taker_fee_denominator
-        );
-        
-        
-        _updateFilledState(
-            makerOrderInfo.orderHash,
-            matchedFillResults.takerSellFilledAmount
-        );
-
-        _updateFilledState(
-            takerOrderInfo.orderHash,
-            matchedFillResults.makerSellFilledAmount
-        );
-
-        _settleMatchedOrders(
-            makerOrder,
-            takerOrder,
-            matchedFillResults
-        );
-
-        return matchedFillResults;
-   }
-
-
-
-    function _settleMatchedOrders(
-        LibOrder.Order memory makerOrder,
-        LibOrder.Order memory takerOrder,
-        LibFillResults.MatchedFillResults memory matchedFillResults
-    )
-    internal{
-        require(
-            IERC20(takerOrder.sellToken).balanceOf(takerOrder.user) >= matchedFillResults.takerSellFilledAmount,
-            "taker order not enough balance"
-        );
-        require(
-            IERC20(makerOrder.sellToken).balanceOf(makerOrder.user) >= matchedFillResults.makerSellFilledAmount,
-            "maker order not enough balance"
-        );
-        
-        // Right maker asset -> maker maker
-        IERC20(takerOrder.sellToken).transferFrom(takerOrder.user, makerOrder.user, matchedFillResults.takerSellFilledAmount);
-       
-        // Left maker asset -> taker maker
-        IERC20(makerOrder.sellToken).transferFrom(makerOrder.user, takerOrder.user, matchedFillResults.makerSellFilledAmount);
-
-
-        /*
-            Fees Paid 
-        */
-        // Taker fee + gas fee -> fee recipient
-        uint takerOrderFees = matchedFillResults.takerFeePaid + takerOrder.gasFee;
-        if (takerOrderFees > 0) {
-            require(
-                IERC20(takerOrder.sellToken).balanceOf(takerOrder.user) >= takerOrderFees,
-                "taker order not enough balance for fee"
-            );
-            IERC20(takerOrder.sellToken).transferFrom(takerOrder.user, FEE_ADDRESS, takerOrderFees);
-        }
-       
-        // Maker fee -> fee recipient
-        if (matchedFillResults.makerFeePaid > 0) {
-            require(
-                IERC20(makerOrder.sellToken).balanceOf(makerOrder.user) >= matchedFillResults.makerFeePaid,
-                "maker order not enough balance for fee"
-            );
-            IERC20(makerOrder.sellToken).transferFrom(makerOrder.user, FEE_ADDRESS, matchedFillResults.makerFeePaid);
-        }
-
-        emit Swap(makerOrder.user, takerOrder.user, makerOrder.sellToken, takerOrder.sellToken, matchedFillResults.makerSellFilledAmount, matchedFillResults.takerSellFilledAmount, takerOrder.gasFee, matchedFillResults.makerFeePaid, matchedFillResults.takerFeePaid);
-
-    }
-
-
-    function _updateFilledState(bytes32 orderHash, uint256 orderBuyFilledAmount) internal{
-       
-        filled[orderHash] += orderBuyFilledAmount;
-    }
-
-    function getOrderInfo(LibOrder.Order memory order) public view returns(LibOrder.OrderInfo memory orderInfo){
-        (orderInfo.orderHash, orderInfo.orderBuyFilledAmount) = _getOrderHashAndFilledAmount(order);
-        
-        require(order.sellAmount > 0, "invalid maker asset amount");
-        require(order.buyAmount > 0, "invalid taker asset amount");
-        require(orderInfo.orderBuyFilledAmount < order.buyAmount, "order is filled");
-        require(block.timestamp <= order.expirationTimeSeconds, "order expired");
-        require(!cancelled[orderInfo.orderHash], "order canceled");
-
-        orderInfo.orderStatus = LibOrder.OrderStatus.FILLABLE;
-        return orderInfo;
-    }
-
-    function _getOrderHashAndFilledAmount(LibOrder.Order memory order)
-        internal
-        view
-        returns (bytes32 orderHash, uint256 orderBuyFilledAmount)
-    {
-        orderHash = order.getOrderHash();
-        orderBuyFilledAmount = filled[orderHash];
-        return (orderHash, orderBuyFilledAmount);
-    }
-
-    function setFees (
-        uint _taker_fee_numerator,
-        uint _taker_fee_denominator,
-        uint _maker_fee_numerator,
-        uint _maker_fee_denominator
-    ) public{   
-        require(msg.sender == FEE_ADDRESS, "only fee address may update fees");
-
-        taker_fee_numerator = _taker_fee_numerator;
-        taker_fee_denominator = _taker_fee_denominator;
-        maker_fee_numerator = _maker_fee_numerator;
-        maker_fee_denominator = _maker_fee_denominator;
-    }
-
+    taker_fee_numerator = _taker_fee_numerator;
+    taker_fee_denominator = _taker_fee_denominator;
+    maker_fee_numerator = _maker_fee_numerator;
+    maker_fee_denominator = _maker_fee_denominator;
+  }
 }
