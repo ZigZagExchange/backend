128cdaf60399fa0a4ecf0f8f018462067f6c4d7e
Trooper <trooper.crypto7@gmail.com>
2022-11-02 23:14:03 +0100

    test new workflow2

--- evm_contracts/test/utils/SignUtil.ts
+++ evm_contracts/test/utils/SignUtil.ts
@@ -30,7 +30,7 @@ export async function signOrder(
     primaryType: 'Order',
     domain: {
       name: 'ZigZag',
-      version: '2.0',
+      version: '2.5',
       chainId: '31337', // test hardhat default
       verifyingContract: exchangeAddress,
     },
