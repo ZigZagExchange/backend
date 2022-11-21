f9046a1b1ed9a6ba033238e8dce69811a06e9583
Alexey <alexey-032@yandex.ru>
2022-11-21 23:19:25 +0300

    improves updateMarketSummarys performance

--- src/background.ts
+++ src/background.ts
@@ -233,35 +233,35 @@ async function updatePendingOrders() {
   console.timeEnd('updatePendingOrders')
 }
 
-async function getPricesInfo(chainId: number) {
-  const redisRes = await Promise.allSettled([
-    redis.HGETALL(`lastprices:${chainId}`),
-    redis.HGETALL(`volume:${chainId}:quote`),
-    redis.HGETALL(`volume:${chainId}:base`),
-    redis.SMEMBERS(`activemarkets:${chainId}`),
-  ])
-
-  const failedRes: any[] = redisRes
-    .filter((x): x is PromiseRejectedResult => x.status === 'rejected')
-    .map((x) => x.reason)
-
-  if (failedRes.length > 0) {
-    throw new Error('Some prices info is lost!')
-  }
-
-  return redisRes
-    .filter((x): x is PromiseFulfilledResult<any> => x.status === 'fulfilled')
-    .map((x) => x.value)
-}
-
 async function updateLastPrices() {
   console.time('updateLastPrices')
+
+  async function getData(chainId: number) {
+    const redisRes = await Promise.allSettled([
+      redis.HGETALL(`lastprices:${chainId}`),
+      redis.HGETALL(`volume:${chainId}:quote`),
+      redis.HGETALL(`volume:${chainId}:base`),
+      redis.SMEMBERS(`activemarkets:${chainId}`),
+    ])
+
+    const failedRes: any[] = redisRes
+      .filter((x): x is PromiseRejectedResult => x.status === 'rejected')
+      .map((x) => x.reason)
+
+    if (failedRes.length > 0) {
+      throw new Error('Some prices info is lost!')
+    }
+
+    return redisRes
+      .filter((x): x is PromiseFulfilledResult<any> => x.status === 'fulfilled')
+      .map((x) => x.value)
+  }
 
   const results0: Promise<any>[] = VALID_CHAINS.map(async (chainId) => {
     const redisKeyPriceInfo = `lastpriceinfo:${chainId}`
 
     const [redisPrices, redisPricesQuote, redisVolumesBase, redisMarkets] =
-      await getPricesInfo(chainId)
+      await getData(chainId)
 
     const results1: Promise<any>[] = (redisMarkets as any[]).map(
       async (marketId) => {
@@ -296,27 +296,54 @@ async function updateLastPrices() {
 async function updateMarketSummarys() {
   console.time('updateMarketSummarys')
 
+  async function getData(chainId: number) {
+    const redisRes = await Promise.allSettled([
+      redis.HGETALL(`volume:${chainId}:quote`),
+      redis.HGETALL(`volume:${chainId}:base`),
+      redis.HGETALL(`lastprices:${chainId}`),
+      redis.HGETALL(`price:${chainId}:low`),
+      redis.HGETALL(`price:${chainId}:high`),
+      redis.HGETALL(`bestask:${chainId}`),
+      redis.HGETALL(`bestbid:${chainId}`),
+      redis.SMEMBERS(`activemarkets:${chainId}`),
+      redis.HGETALL(`volume:utc:${chainId}:quote`),
+      redis.HGETALL(`volume:utc:${chainId}:base`),
+      redis.HGETALL(`price:utc:${chainId}:low`),
+      redis.HGETALL(`price:utc:${chainId}:high`),
+    ])
+
+    const failedRes: any[] = redisRes
+      .filter((x): x is PromiseRejectedResult => x.status === 'rejected')
+      .map((x) => x.reason)
+
+    if (failedRes.length > 0) {
+      throw new Error('Some prices info is lost!')
+    }
+
+    return redisRes
+      .filter((x): x is PromiseFulfilledResult<any> => x.status === 'fulfilled')
+      .map((x) => x.value)
+  }
+
   const results0: Promise<any>[] = VALID_CHAINS.map(async (chainId) => {
     const redisKeyMarketSummary = `marketsummary:${chainId}`
     const redisKeyMarketSummaryUTC = `marketsummary:utc:${chainId}`
 
     // fetch needed data
-    const redisVolumesQuote = await redis.HGETALL(`volume:${chainId}:quote`)
-    const redisVolumesBase = await redis.HGETALL(`volume:${chainId}:base`)
-    const redisPrices = await redis.HGETALL(`lastprices:${chainId}`)
-    const redisPricesLow = await redis.HGETALL(`price:${chainId}:low`)
-    const redisPricesHigh = await redis.HGETALL(`price:${chainId}:high`)
-    const redisBestAsk = await redis.HGETALL(`bestask:${chainId}`)
-    const redisBestBid = await redis.HGETALL(`bestbid:${chainId}`)
-    const markets = await redis.SMEMBERS(`activemarkets:${chainId}`)
-    const redisVolumesQuoteUTC = await redis.HGETALL(
-      `volume:utc:${chainId}:quote`
-    )
-    const redisVolumesBaseUTC = await redis.HGETALL(
-      `volume:utc:${chainId}:base`
-    )
-    const redisPricesLowUTC = await redis.HGETALL(`price:utc:${chainId}:low`)
-    const redisPricesHighUTC = await redis.HGETALL(`price:utc:${chainId}:high`)
+    const [
+      redisVolumesQuote,
+      redisVolumesBase,
+      redisPrices,
+      redisPricesLow,
+      redisPricesHigh,
+      redisBestAsk,
+      redisBestBid,
+      markets,
+      redisVolumesQuoteUTC,
+      redisVolumesBaseUTC,
+      redisPricesLowUTC,
+      redisPricesHighUTC,
+    ] = await getData(chainId)
 
     const results1: Promise<any>[] = markets.map(async (marketId: ZZMarket) => {
       const marketInfo = await getMarketInfo(marketId, chainId).catch(
