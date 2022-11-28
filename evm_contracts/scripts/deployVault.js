7a534b7ad11cc41569cbbb253010973e3b3ea379
taureau75 <vixidev99@protonmail.com>
2022-11-28 13:02:09 -0800

    change vault name

--- evm_contracts/scripts/deployVault.js
+++ evm_contracts/scripts/deployVault.js
@@ -3,7 +3,7 @@ const hre = require('hardhat')
 async function main() {
   const Vault = await hre.ethers.getContractFactory('ZigZagVault')
   const manager = "0x6f457Ce670D18FF8bda00E1B5D9654833e7D91BB";
-  const vault = await Vault.deploy(manager, "ZigZag LP 1", "ZZLP1");
+  const vault = await Vault.deploy(manager, "ZigZag LP", "ZZLP");
 
   await vault.deployed()
 
