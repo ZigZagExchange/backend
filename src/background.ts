91ad6e856ea6428d7c7967591e9ef0b6f83aa72f
taureau75 <vixidev99@protonmail.com>
2023-01-28 11:28:21 -0800

    Revert "try once more to fix last price"
    This reverts commit 78511268472d0b62a17eed4b2e3ea9a879fffd37.

--- src/background.ts
+++ src/background.ts
@@ -347,7 +347,7 @@ async function removeOldLiquidity() {
       const asks = new Array(lenghtAsks)
       const bids = new Array(lengthBids)
 
-      // Bucket bids and asks
+      // Update last price
       let askPrice = 0
       let askAmount = 0
       let bidPrice = 0
@@ -367,8 +367,10 @@ async function removeOldLiquidity() {
           Number(uniqueBuy[bidSet[bidSet.length - i]]),
         ]
       }
+      const mid = (askPrice / askAmount + bidPrice / bidAmount) / 2
+      console.log(mid);
 
-      // Store best bids and asks per market
+      // Store best bids, asks, and mid per market
       const bestAskPrice = asks[0]?.[1] ? asks[0][1] : '0'
       const bestBidPrice = bids[0]?.[1] ? bids[0][1] : '0'
       const bestLiquidity = asks.concat(bids)
@@ -379,9 +381,6 @@ async function removeOldLiquidity() {
         JSON.stringify(bestLiquidity),
         { EX: 45 }
       )
-
-      // Update last price
-      const mid = (Number(bestAskPrice) + Number(bestBidPrice)) / 2
       if (!Number.isNaN(mid) && mid > 0) {
         redis.HSET(`lastprices:${chainId}`, marketId, formatPrice(mid))
       }
